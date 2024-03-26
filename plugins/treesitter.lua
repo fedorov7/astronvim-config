8dfc9b02ecaa526230a8f0c4e8c4a9450282e1e1
Alexander Fedorov <fedorov7@gmail.com>
2024-03-26 10:28:16 +0300

    feat(plugins): add treesitter customization

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,22 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "bash",
+      "c",
+      "cpp",
+      "csv",
+      "gitignore",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "rust",
+      "sql",
+      "toml",
+      "vim",
+      "yaml",
     })
   end,
 }
